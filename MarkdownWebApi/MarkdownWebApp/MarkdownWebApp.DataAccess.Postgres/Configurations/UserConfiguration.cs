9ef422e4c047b6b8c4af653c0b662f87c769cdb7
MaratAbashev <maratabashev@yandex.ru>
2025-01-24 23:14:05 +0300

    Добавлена бд (с миграциями, пока без репозиториев)

---
+++ MarkdownWebApi/MarkdownWebApp/MarkdownWebApp.DataAccess.Postgres/Configurations/UserConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using MarkdownWebApp.DataAccess.Postgres.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MarkdownWebApp.DataAccess.Postgres.Configurations;
+
+public class UserConfiguration: IEntityTypeConfiguration<UserEntity>
+{
+    public void Configure(EntityTypeBuilder<UserEntity> builder)
+    {
+        builder
+            .HasKey(u => u.Id);
+        builder
+            .HasMany(u => u.UserDocuments)
+            .WithOne(da => da.User)
+            .HasForeignKey(da => da.UserId);
+    }
+}
